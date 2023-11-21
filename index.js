2720b63c285fc54edcf16e66f02a07411b28a8a9
rashidsarkar <rashidrock558@gmail.com>
2023-11-22 01:43:28 +0600

    Added an endpoint to get order statistics using aggregate pipeline. (generated by blackbox.ai )

--- index.js
+++ index.js
@@ -279,6 +279,50 @@ async function run() {
       res.send({ users, menuItem, orders, revenue });
     });
 
+    // using aggregate pipeline
+    app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
+      try {
+        const result = await paymentCollection
+          .aggregate([
+            {
+              $unwind: "$menuItemIds",
+            },
+            {
+              $lookup: {
+                from: "menu",
+                localField: "menuItemIds",
+                foreignField: "_id",
+                as: "menuItems",
+              },
+            },
+            {
+              $unwind: "$menuItems",
+            },
+            {
+              $group: {
+                _id: "$menuItems.category",
+                quantity: { $sum: 1 },
+                revenue: { $sum: "$menuItems.price" },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                category: "$_id",
+                quantity: "$quantity",
+                revenue: "$revenue",
+              },
+            },
+          ])
+          .toArray();
+        res.send(result);
+      } catch (error) {
+        // sent res error massage
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
